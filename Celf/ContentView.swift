9227a375b079f5f2063e4c5b2daa1c12973ef736
chrisdholmes1 <chrisdholmes1@gmail.com>
2020-08-31 13:07:16 -0700

    Created icon for CELF. Added a login screen using swift UI. Created a subview for a CustomButton in swift UI. Left print statements as place holders for the Login and Register buttons.

--- Celf/ContentView.swift
+++ Celf/ContentView.swift
@@ -10,12 +10,51 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+
+        VStack{
+            Spacer()
+            
+            Image("celf").resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 300, height:300)
+            
+            Spacer()
+            
+            CustomButtonView(text: "Register"){
+                print("Register Button")
+            }
+            
+            Divider().hidden()
+            
+            CustomButtonView(text: "Login"){
+                print("Login Button")
+            }
+            
+            
+            Spacer()
+        }
+ 
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView().previewDevice("iPhone XR")
+    }
+}
+
+
+
+struct CustomButtonView: View {
+    let text: String
+    let action: () -> ()
+    var body: some View {
+        Button(action: action) {
+            Text(text)
+                .foregroundColor(Color.white)
+                .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+        }
+        .frame(width: 300, height: 75)
+        .background(Color.brandBlue)
     }
 }
---
+++ Celf/K.swift
@@ -0,0 +1,18 @@
+//
+//  K.swift
+//  Celf
+//
+//  Created by pancake on 8/31/20.
+//  Copyright © 2020 Christopher Holmes. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import SwiftUI
+
+
+extension Color {
+    
+    static let brandBlue = Color("brandBlue")
+}
+
