88a4b13d5803ec59d313f0fd1acff78c6e1ce816
chrisdholmes1 <chrisdholmes1@gmail.com>
2020-08-31 22:22:23 -0700

    Updated quote manager to return a random hard coded quote from an array. When user opens login screen they will see a different motivational quote.

--- Celf/model/QuoteManager.swift
+++ Celf/model/QuoteManager.swift
@@ -10,6 +10,22 @@ import Foundation
 
 
 class QuoteManager {
+    var quote: Quote? {
+        let rand = Int.random(in:0...2)
+        
+        let quotes = [
+            Quote(speaker: "Malcolm X", quote: "Education is the passport to the future."),
+            Quote(speaker:"Chadwick Boseman",
+            quote: "Sometimes you need to feel the pain and sting of defeat to activate the real passion and purpose that god predestined inside of you."),
+            Quote(speaker: "Justin Perry",quote: "I trust the timing of the Universe")
+        ]
+        
+        return quotes[rand]
+    }
     
+    
+    func getQuote() -> Quote?{
+        return quote
+    }
     
 }
