0e8d9cf5057956848bf4a83273382a50270b35d8
chrisdholmes1 <chrisdholmes1@gmail.com>
2020-09-01 13:43:51 -0700

    Journal Entry View displays to the user their journal entry.

---
+++ Celf/view/JournalEntryView.swift
@@ -0,0 +1,37 @@
+//
+//  JournalEntryView.swift
+//  Celf
+//
+//  Created by pancake on 9/1/20.
+//  Copyright © 2020 Christopher Holmes. All rights reserved.
+//
+
+import SwiftUI
+
+struct JournalEntryView: View {
+    var title: String
+    var bodyText: String
+    var body: some View {
+        
+       
+            VStack(alignment: .leading){
+                Text(title)
+                    .font(.title)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding(.bottom)
+                
+                
+                Text(bodyText)
+                    .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
+            }.frame(width: 350, height: 200)
+    
+    }
+    
+}
+
+
+struct JournalEntryView_Previews: PreviewProvider {
+    static var previews: some View {
+        JournalEntryView(title: "Event - Title", bodyText: "I made a million dollar today baby.")
+    }
+}
