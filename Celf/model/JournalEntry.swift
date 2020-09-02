780ec253af3beb3a8934ba909e187ed0b3c36670
chrisdholmes1 <chrisdholmes1@gmail.com>
2020-09-01 20:51:31 -0700

    JournalEntry class created. Will be used for login the user's journal entries.

---
+++ Celf/model/JournalEntry.swift
@@ -0,0 +1,22 @@
+//
+//  JournalEntry.swift
+//  Celf
+//
+//  Created by pancake on 9/1/20.
+//  Copyright © 2020 Christopher Holmes. All rights reserved.
+//
+
+import Foundation
+
+struct JournalEntry{
+    var title: String?
+    var bodyText: String?
+    var date: String?
+    
+    /**
+     Journal Entry may need limits to the title - character limits for the
+     Title. The date should be grabbed from the system here in the struct some how.
+       The body text may also need a character limit.
+     */
+    
+}
