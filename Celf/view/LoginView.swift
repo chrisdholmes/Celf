1b1fd99a6d5a05d77afb04e366a8f013baa92159
chrisdholmes1 <chrisdholmes1@gmail.com>
2020-09-01 20:46:04 -0700

    Added Navigation Link to LoginView Login Button - customized navigation view so when BottomBarView is displayed it has the Celf logo and a gear icon for future a future settings page.

--- Celf/view/LoginView.swift
+++ Celf/view/LoginView.swift
@@ -9,23 +9,24 @@
 import SwiftUI
 
 /**
-    LoginView  file will allow a user to login to the Celf App
-      via FireBase.
-
-     TODO
-       - update to include account login via firebase
-       - only allow login button to be available once all fields are done
-       - throw error is log in fails - such as bad password - no internet etc
+ LoginView  file will allow a user to login to the Celf App
+ via FireBase.
  
-
-*/
+ TODO
+ - update to include account login via firebase
+ - only allow login button to be available once all fields are done
+ - throw error is log in fails - such as bad password - no internet etc
+ 
+ 
+ */
 
 struct LoginView: View {
     @State private var userName: String = ""
     @State private var password: String = ""
+    @State private var navLinkTag: Int? = 0
     
     var body: some View {
-    
+        
         VStack(){
             
             Image("celf")
@@ -39,25 +40,44 @@ struct LoginView: View {
                 .font(.title)
                 .padding(.horizontal)
             Divider()
-            .frame(height: 2)
-            .background(Color.brandBlue)
-            .frame(width: 300)
+                .frame(height: 2)
+                .background(Color.brandBlue)
+                .frame(width: 300)
             
-            TextField("password", text: $password)
+            SecureField("password", text: $password)
                 .frame(width: 300, height: 75)
                 .font(.title)
                 .padding(.horizontal)
             
             Divider()
-            .frame(height: 2)
-            .background(Color.brandBlue)
+                .frame(height: 2)
+                .background(Color.brandBlue)
                 .frame(width: 300)
             
             Divider().hidden().padding(.vertical)
-            CustomButtonView(text: "Login"){
+            
+            NavigationLink(destination: BottomBarView()
+                .navigationBarBackButtonHidden(true)
+                .navigationBarItems(
+                    leading: Image("celf").resizable().aspectRatio(contentMode: .fit)
+                        .frame(width: 30, height: 30),
+                    trailing:
+                        Image(systemName: "gear")
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 30, height: 30))
+                .navigationBarTitle(
+                    Text("Celf")
+                    ,displayMode:.inline), tag: 1, selection: $navLinkTag){
+                        EmptyView()
+                        
+            }
+            CustomButtonView(text: "Login")
+            {
                 print("LoginView - Log in Button")
+                self.navLinkTag = 1
             }
         }
+        
     }
 }
 
