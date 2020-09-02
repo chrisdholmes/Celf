f2369f80600adb4157b7dff74f32c1ed8a02bf06
chrisdholmes1 <chrisdholmes1@gmail.com>
2020-09-01 20:56:37 -0700

    Renamed DayEventView to DayCardView - DayCardView seems a more appropriate name since - a Day is a collection of Event. Card comes from the Android CardView.

--- Celf/view/HomeView.swift
+++ Celf/view/HomeView.swift
@@ -8,20 +8,19 @@
 
 import SwiftUI
 /**
-   The HomeView will display several different types of EventViews
-    the different cards will represent a list of actions that were performed.
-     Actions are going to be started, stopped, and tracked in an AddView.
-     The Home view will display in a long list all the actions that have previously
- occurred in chronological order. As the different types of events occur in the AddView
- are updated to the App - the HomeView will udpate alongside it.
+ The HomeView will display several different types of EventViews
+ the different cards will represent a list of actions that were performed.
+ Actions are going to be started, stopped, and tracked in an AddView.
+ The Home view will display in a long list all the actions that have previously
+ occurred in chronological order. As the different types of events occur in the AddView are updated to the App - the HomeView will udpate alongside it.
  At the end of the day - the user should perform a journal entry to write about
  how their day went, reflect on progress or just express themselves. 
  */
 struct HomeView: View {
-
-     var body: some View {
+    
+    var body: some View {
         List{
-            DayEventView()
+            DayCardView()
         }
     }
 }
