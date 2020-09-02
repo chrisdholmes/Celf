363a99600d3ebcf723ca2f8a9d2ee2e4dbf2dd59
chrisdholmes1 <chrisdholmes1@gmail.com>
2020-09-01 20:47:31 -0700

    Badge struct created to store the name of the badge that the user earned. Badge struct will get proper system file name for the logo of the badge earned. Also will store date time of moment badge earned. Possibly, a memo of the moment badge was earned.

---
+++ Celf/model/Badge.swift
@@ -0,0 +1,19 @@
+//
+//  Badge.swift
+//  Celf
+//
+//  Created by pancake on 9/1/20.
+//  Copyright © 2020 Christopher Holmes. All rights reserved.
+//
+
+import Foundation
+/**
+    Not sure if I need a badge class for the badges as the badgeName
+    is just a string at this time. In the future I may need an actual badge class
+     if the badges become more complicated.
+     
+      
+ */
+struct Badge{
+    let badgeName: String
+}
