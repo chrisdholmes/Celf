dceb37c376a3211861305f239744d02b863cd077
chrisdholmes1 <chrisdholmes1@gmail.com>
2020-09-01 20:50:04 -0700

    EventCardViewManager will manage the (currently empty) types of events the user logs. They will be displayed in the HomeView ListView along with the current DayCard view.

---
+++ Celf/model/EventCardViewManager.swift
@@ -0,0 +1,13 @@
+//
+//  EventCardViewManager.swift
+//  Celf
+//
+//  Created by pancake on 9/1/20.
+//  Copyright © 2020 Christopher Holmes. All rights reserved.
+//
+
+import Foundation
+
+class EventCardViewManager: ObservableObject{
+    
+}
