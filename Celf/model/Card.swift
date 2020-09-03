f62e7c8da2286055fe8d097758c3f0d0a1b7db7e
chrisdholmes1 <chrisdholmes1@gmail.com>
2020-09-02 21:00:10 -0700

    Card is a class that is a superclass for the DayCard and EventCard classes. Card uses a Date() and DateFormatter() object to retrieve the date and team at the moment of the cards initialization. Also generated an ID based on the timeInterval. And Initializes an empty JournalEntry object.

---
+++ Celf/model/Card.swift
@@ -0,0 +1,38 @@
+//
+//  Card.swift
+//  Celf
+//
+//  Created by pancake on 9/2/20.
+//  Copyright © 2020 Christopher Holmes. All rights reserved.
+//
+
+import Foundation
+
+class Card: Identifiable{
+    
+    private let today = Date()
+    private let formatter = DateFormatter()
+    
+    init() {
+        print("New Card Initialized")
+    }
+    
+    var id: Int {
+        return Int(today.timeIntervalSince1970 * 10000000)
+    }
+    var date: String {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "MM/dd/yyyy"
+        return formatter.string(from: today)
+    }
+    var time: String{
+        formatter.dateFormat = "h:mm a"
+        formatter.amSymbol = "AM"
+        formatter.pmSymbol = "PM"
+        
+        return formatter.string(from: today)
+    }
+    var entry: JournalEntry = JournalEntry()
+    
+    
+}
