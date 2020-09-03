28c46b0d94333a8990f8d02c7035f9fd0b7ec55d
chrisdholmes1 <chrisdholmes1@gmail.com>
2020-09-02 21:05:56 -0700

    Updated Badge class to have an id so it is identifiable in potential list views in the future. Used computed values for the colorName and imageName of the Badge to retrieve the proper system name for the SF Symbol to be displayed.

--- Celf/model/Badge.swift
+++ Celf/model/Badge.swift
@@ -8,14 +8,51 @@
 
 import Foundation
 /**
-    Not sure if I need a badge class for the badges as the badgeName
-    is just a string at this time. In the future I may need an actual badge class
-     if the badges become more complicated.
-     
-      
+ Not sure if I need a badge class for the badges as the badgeName
+ is just a string at this time. In the future I may need an actual badge class
+ if the badges become more complicated.
+ 
+ 
  */
-struct Badge{
-    let badgeName: String
-    let date: String
-    let memo: String
+
+import UIKit
+import SwiftUI
+
+struct Badge: Identifiable{
+    
+    var id: Int {
+        let date = Date()
+        return Int(date.timeIntervalSince1970 * 10000000)
+    }
+    let category: String
+    
+    var colorName: Color{
+        var result = Color.brandBlue
+        switch category{
+        case K.meditation : result = Color.meditationViolet;
+        case K.journal: result = Color.journalBlue;
+        case K.talk: result = Color.talkGreen;
+        case K.exercise: result = Color.exerciseRed
+        case K.conquer: result = Color.conquerYellow;
+        default: result = Color.brandBlue
+        }
+        
+        return result
+    }
+    
+    var imageName: String {
+        var result = ""
+        switch category{
+        case K.meditation: result = "tuningfork";
+        case K.journal: result = "book.circle.fill";
+        case K.talk: result = "person.2.fill";
+        case K.exercise: result = "heart.circle.fill";
+        case K.conquer: result = "bolt.circle.fill";
+        default: result = "square"
+        }
+        
+        return result
+    }
+    
+    
 }
