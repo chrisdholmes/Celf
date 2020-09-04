f1d497d43b59d643e910ccb29d7f6f445d3f8442
chrisdholmes1 <chrisdholmes1@gmail.com>
2020-09-04 12:04:11 -0700

    Added a method to the Badge class that provides a new line character for every space  for the Distortion Cards to display properly in the DistortionInUpdateView class.

--- Celf/model/Badge.swift
+++ Celf/model/Badge.swift
@@ -25,7 +25,8 @@ struct Badge: Identifiable{
         return Int(date.timeIntervalSince1970 * 10000000)
     }
     let category: String
-    
+    //TODO use K.constants to update the colorName instead of
+    // using this right here - this is not that good.
     var colorName: Color{
         var result = Color.brandBlue
         switch category{
@@ -34,7 +35,7 @@ struct Badge: Identifiable{
         case K.talk: result = Color.talkGreen;
         case K.exercise: result = Color.exerciseRed
         case K.conquer: result = Color.conquerYellow;
-        default: result = Color.brandBlue
+        default: result = Color.distortionBlack
         }
         
         return result
@@ -44,15 +45,34 @@ struct Badge: Identifiable{
         var result = ""
         switch category{
         case K.meditation: result = "tuningfork";
-        case K.journal: result = "book.circle.fill";
-        case K.talk: result = "person.2.fill";
-        case K.exercise: result = "heart.circle.fill";
-        case K.conquer: result = "bolt.circle.fill";
-        default: result = "square"
+        case K.journal: result = "book.circle";
+        case K.talk: result = "person.circle";
+        case K.exercise: result = "heart.circle";
+        case K.conquer: result = "bolt.circle";
+        default: result = "d.circle"
         }
         
         return result
     }
     
+    func getCategoryForUpdateView() -> String
+    {
+        let arr = self.category.split(separator: " ")
+        
+        var result = ""
+        
+        for i in arr {
+            result += i + "\n"
+        }
+        
+        return result.uppercased()
+        
+    }
     
+}
+
+struct Badge_Previews: PreviewProvider {
+    static var previews: some View {
+        /*@START_MENU_TOKEN@*/Text("Hello, World!")/*@END_MENU_TOKEN@*/
+    }
 }
